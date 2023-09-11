4f11409d3f1057973b63a01a93c77c34b599473b
madvorak <martin.dvorak@matfyz.cz>
2023-09-11 16:21:30 +0200

    dumpster fire

--- Thue/Multitape/Definition.lean
+++ Thue/Multitape/Definition.lean
@@ -1,60 +1,63 @@
 import Mathlib.Computability.Language
-import Mathlib.Util.PiNotation
-import Mathlib.Data.Fin.Tuple.Basic
+import Mathlib.Data.Prod.TProd
 
 
-def Co {k : ℕ} (β : Fin k → Type) : Type :=
-  Π i : Fin k, List (β i)
+@[reducible]
+def Tapes (k : ℕ) (τ : ℕ → Type) : Type :=
+  List.TProd (List ∘ τ) (List.range k)
 
 /-- Rewrite rule -/
-structure Mrule {k : ℕ} (β : Fin k → Type) where
-  inputs : Co β
-  outputs : Co β
+structure Mrule (k : ℕ) (τ : ℕ → Type) where
+  inputs : Tapes k τ
+  outputs : Tapes k τ
 
 /-- Multi-tape semi-Thue system (or "multi-string rewriting system") -/
-structure Multi (α : Type) where        -- alphabet for words
-  k : ℕ                                 -- number of tapes
-  k_pos : NeZero k                      -- at least one tape
-  β : Fin k → Type                      -- tape alphabets
-  embed : α → β 0                       -- embedding words onto the input tape
-  ruleset : List (Mrule β)              -- rewrite rules
-  starting : Co β                       -- initialization strings
-  accepting : Π i : Fin k, Option (β i) -- accepting / halting condition(s)
+structure Multi (α : Type) where    -- alphabet for words
+  k : ℕ                             -- number of tapes
+  τ : ℕ → Type                      -- tape alphabets
+  embed : α → τ 0                   -- embedding words onto the top tape
+  ruleset : List (Mrule k τ)        -- rewrite rules
+  starting : Tapes k τ              -- initialization strings
+  accepting : ∀ i : ℕ, Option (τ i) -- accepting / halting condition(s)
 
 
 variable {α : Type}
 
 /-- Initialize `M` with input `w` on which the computation should be done. -/
-def Multi.initiate (M : Multi α) (w : List α) : Co M.β :=
-  let _ : NeZero M.k := M.k_pos ;
-  Function.update M.starting 0 (M.starting 0 ++ w.map M.embed)
+def Multi.initiate (M : Multi α) (w : List α) : Tapes M.k M.τ :=
+M.starting -- Function.update M.starting 0 (M.starting 0 ++ w.map M.embed) -- TODO
 
 /-- Does `M` consider `s` to be in accepting state? -/
-def Multi.terminate (M : Multi α) (s : Co M.β) : Prop :=
-  ∀ i : Fin M.k, M.accepting i = none ∨ M.accepting i = (s i).head?
+def Multi.terminate (M : Multi α) (s : Tapes M.k M.τ) : Prop :=
+  ∀ i : ℕ, (ok : i < M.k) →
+    M.accepting i = none ∨ M.accepting i = (s.elim (Iff.mpr List.mem_range ok)).head?
 
 
 /-- One rewriting step. -/
-def Multi.Transforms (M : Multi α) (x y : Co M.β) : Prop :=
-  ∃ r ∈ M.ruleset, ∀ i : Fin M.k,
-    ∃ u v : List (M.β i), x i = u ++ r.inputs i ++ v ∧ y i = u ++ r.outputs i ++ v
+def Multi.Transforms (M : Multi α) (x y : Tapes M.k M.τ) : Prop :=
+  ∃ r ∈ M.ruleset, ∀ i : ℕ, (ok : i < M.k) →
+    ∃ u v : List (M.τ i),
+      let oki := Iff.mpr List.mem_range ok;
+      let rᵢ : List (M.τ i) := r.inputs.elim oki;
+      let rₒ : List (M.τ i) := r.outputs.elim oki;
+      x.elim oki = u ++ rᵢ ++ v ∧ y.elim oki = u ++ rₒ ++ v
 
 /-- Closure (reflexive+transitive) of `M.Transforms` with step counting. Predicate `M.Derives s t n` means
     the multi-tape semi-Thue system `M` can transform `s` to `t` in exactly `n` rewriting step. -/
-inductive Multi.Derives (M : Multi α) : Co M.β → Co M.β → ℕ → Prop
-  | refl (x : Co M.β) : M.Derives x x 0
-  | tail (x y z : Co M.β) (n : ℕ) : M.Derives x y n → M.Transforms y z → M.Derives x z n.succ
+inductive Multi.Derives (M : Multi α) : Tapes M.k M.τ → Tapes M.k M.τ → ℕ → Prop
+  | refl (x : Tapes M.k M.τ) : M.Derives x x 0
+  | tail (x y z : Tapes M.k M.τ) (n : ℕ) : M.Derives x y n → M.Transforms y z → M.Derives x z n.succ
 
 /-- Language recognized by given multi-tape semi-Thue system. -/
 def Multi.Semidecides (M : Multi α) : Language α :=
-  { w | ∃ n : ℕ, ∃ s : Co M.β, M.Derives (M.initiate w) s n ∧ M.terminate s }
+  { w | ∃ n : ℕ, ∃ s : Tapes M.k M.τ, M.Derives (M.initiate w) s n ∧ M.terminate s }
 
 /-- Definition of recursively-enumerable languages in terms of multi-tape semi-Thue systems. -/
 def Language.InMRE (L : Language α) : Prop :=
   ∃ M : Multi α, M.Semidecides = L
 
 
-/-- Related concept: https://en.wikipedia.org/wiki/Unambiguous_Turing_machine -/
+/-- Semantic definition of deterministic computation. -/
 def Multi.IsDeterministic (M : Multi α) : Prop :=
-  ∀ w : List α, ∀ n : ℕ, ∀ s : Co M.β, M.Derives (M.initiate w) s n →
-    ∀ x y : Co M.β, M.Transforms s x ∧ M.Transforms s y → x = y
+  ∀ w : List α, ∀ n : ℕ, ∀ s : Tapes M.k M.τ, M.Derives (M.initiate w) s n →
+    ∀ x y : Tapes M.k M.τ, M.Transforms s x ∧ M.Transforms s y → x = y
--- Thue/Multitape/Examples.lean
+++ Thue/Multitape/Examples.lean
@@ -1,7 +1,51 @@
 import Thue.Multitape.Definition
 import Thue.Multitape.Languages
 
+@[reducible]
+private def tau : ℕ → Type
+| .zero => Option (Fin 2)
+| .succ _ => Fin 6
+
+/-
+Start:
+∅w = ∅v∅v
+225
+
+Accept:
+∅
+45
+-/
+
+private def copy0 : Mrule 2 tau :=
+  Mrule.mk (([none, some 0], [2], ())) (([none], [0, 2], ()))
+
+private def copy1 : Mrule 2 tau :=
+  Mrule.mk (([none, some 1], [2], ())) (([none], [1, 2], ()))
+
+private def uturn : Mrule 2 tau :=
+  Mrule.mk (([none, none], [2], ())) (([none, none], [3], ()))
+
+private def rewind0 : Mrule 2 tau :=
+  Mrule.mk (([none, none], [0, 3], ())) (([none, none], [3], ()))
+
+private def rewind1 : Mrule 2 tau :=
+  Mrule.mk (([none, none], [1, 3], ())) (([none, none], [3], ()))
+
+private def ahead : Mrule 2 tau :=
+  Mrule.mk (([none, none], [2, 3], ())) (([none], [4], ()))
+
+private def check0 : Mrule 2 tau :=
+  Mrule.mk (([none, some 0], [4, 0], ())) (([none], [4], ()))
+
+private def check1 : Mrule 2 tau :=
+  Mrule.mk (([none, some 1], [4, 1], ())) (([none], [4], ()))
+
+private def rulesRep := [copy0, copy1, uturn, rewind0, rewind1, ahead, check0, check1]
+
+private def machineRep : Multi (Option (Fin 2)) :=
+  Multi.mk 2 tau id rulesRep (([none], [2, 2, 5], ())) sorry
 
 example : (langRepetition (Fin 2)).InMRE :=
 by
+  use machineRep
   sorry
