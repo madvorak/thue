282ca3a5d4c8e6ed51a4bc8bd0fdae9618b2ad46
madvorak <martin.dvorak@matfyz.cz>
2023-09-11 16:52:27 +0200

    refactoring the example

--- Thue/Multitape/Examples.lean
+++ Thue/Multitape/Examples.lean
@@ -16,29 +16,32 @@ Accept:
 45
 -/
 
+private def tauRule (u₁ u₂ : List (Option (Fin 2))) (v₁ v₂ : List (Fin 6)) : Mrule 2 tau :=
+  Mrule.mk (u₁, v₁, ()) (u₂, v₂, ())
+
 private def copy0 : Mrule 2 tau :=
-  Mrule.mk (([none, some 0], [2], ())) (([none], [0, 2], ()))
+  tauRule [none, some 0] [none] [2] [0, 2]
 
 private def copy1 : Mrule 2 tau :=
-  Mrule.mk (([none, some 1], [2], ())) (([none], [1, 2], ()))
+  tauRule [none, some 1] [none] [2] [1, 2]
 
 private def uturn : Mrule 2 tau :=
-  Mrule.mk (([none, none], [2], ())) (([none, none], [3], ()))
+  tauRule [none, none] [none, none] [2] [3]
 
 private def rewind0 : Mrule 2 tau :=
-  Mrule.mk (([none, none], [0, 3], ())) (([none, none], [3], ()))
+  tauRule [none, none] [none, none] [0, 3] [3]
 
 private def rewind1 : Mrule 2 tau :=
-  Mrule.mk (([none, none], [1, 3], ())) (([none, none], [3], ()))
+  tauRule [none, none] [none, none] [1, 3] [3]
 
 private def ahead : Mrule 2 tau :=
-  Mrule.mk (([none, none], [2, 3], ())) (([none], [4], ()))
+  tauRule [none, none] [none] [2, 3] [4]
 
 private def check0 : Mrule 2 tau :=
-  Mrule.mk (([none, some 0], [4, 0], ())) (([none], [4], ()))
+  tauRule [none, some 0] [none] [4, 0] [4]
 
 private def check1 : Mrule 2 tau :=
-  Mrule.mk (([none, some 1], [4, 1], ())) (([none], [4], ()))
+  tauRule [none, some 1] [none] [4, 1] [4]
 
 private def rulesRep := [copy0, copy1, uturn, rewind0, rewind1, ahead, check0, check1]
 
