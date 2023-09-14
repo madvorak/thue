2d52784b4bcb5c2bccbde659a339d55d01278747
madvorak <martin.dvorak@matfyz.cz>
2023-09-14 10:57:23 +0200

    off-by-one refactoring

--- Thue/Multitape/Definition.lean
+++ Thue/Multitape/Definition.lean
@@ -7,49 +7,47 @@ def List.TProd.replaceHead {ι : Type} {α : ι → Type} {d : ι} {l : List ι}
 
 
 @[reducible]
-def Tapes (k : ℕ) (τ : ℕ → Type) : Type :=
-  List.TProd (List ∘ τ) (List.range k)
+def Tapes (e : ℕ) (τ : ℕ → Type) : Type :=
+  List.TProd (List ∘ τ) (List.range e.succ)
 
 /-- Rewrite rule -/
-structure Mrule (k : ℕ) (τ : ℕ → Type) where
-  inputs : Tapes k τ
-  outputs : Tapes k τ
+structure Mrule (e : ℕ) (τ : ℕ → Type) where
+  inputs : Tapes e τ
+  outputs : Tapes e τ
 
 /-- Multi-tape semi-Thue system (or "multi-string rewriting system") -/
 structure Multi (α : Type) where -- alphabet for words
-  k : ℕ                          -- number of tapes
-  k_pos : 0 < k                  -- at least one tape
+  e : ℕ                          -- number of extra tapes
   τ : ℕ → Type                   -- tape alphabets
   embed : α → τ 0                -- embedding words onto the top tape
-  ruleset : List (Mrule k τ)     -- rewrite rules
-  starting : Tapes k τ           -- initialization strings
-  accepting : List.TProd (Option ∘ τ) (List.range k) -- accepting / halting condition(s)
+  ruleset : List (Mrule e τ)     -- rewrite rules
+  starting : Tapes e τ           -- initialization strings
+  accepting : List.TProd (Option ∘ τ) (List.range e.succ) -- accepting / halting condition(s)
 
 
 variable {α : Type}
 
-def Multi.oki (M : Multi α) {i : ℕ} (hiMk : i < M.k) : i ∈ List.range M.k := by
-  rwa [List.mem_range]
+def Multi.oki (M : Multi α) {i : ℕ} (hiMk : i ≤ M.e) : i ∈ List.range M.e.succ := by
+  rwa [List.mem_range, Nat.lt_succ]
 
 /-- Initialize `M` with input `w` on which the computation should be done. -/
-def Multi.initiate (M : Multi α) (w : List α) : Tapes M.k M.τ := by
+def Multi.initialize (M : Multi α) (w : List α) : Tapes M.e M.τ := by
   unfold Tapes
-  rw [←Nat.succ_pred_eq_of_pos M.k_pos, List.range_succ_eq_map]
+  rw [List.range_succ_eq_map]
   apply List.TProd.replaceHead
   · rw [←List.range_succ_eq_map]
     convert M.starting
-    exact Nat.succ_pred_eq_of_pos M.k_pos
-  · exact (M.starting.elim (M.oki M.k_pos)).append (w.map M.embed)
+  · exact (M.starting.elim (M.oki (Nat.zero_le M.e))).append (w.map M.embed)
 
 /-- Does `M` consider `s` to be in accepting state? -/
-def Multi.terminate (M : Multi α) (s : Tapes M.k M.τ) : Prop :=
-  ∀ i : ℕ, (ok : i < M.k) →
+def Multi.terminate (M : Multi α) (s : Tapes M.e M.τ) : Prop :=
+  ∀ i : ℕ, (ok : i ≤ M.e) →
     M.accepting.elim (M.oki ok) = none ∨ M.accepting.elim (M.oki ok) = (s.elim (M.oki ok)).head?
 
 
 /-- One rewriting step. -/
-def Multi.Transforms (M : Multi α) (x y : Tapes M.k M.τ) : Prop :=
-  ∃ r ∈ M.ruleset, ∀ i : ℕ, (ok : i < M.k) →
+def Multi.Transforms (M : Multi α) (x y : Tapes M.e M.τ) : Prop :=
+  ∃ r ∈ M.ruleset, ∀ i : ℕ, (ok : i ≤ M.e) →
     ∃ u v : List (M.τ i),
       let rᵢ : List (M.τ i) := r.inputs.elim (M.oki ok);
       let rₒ : List (M.τ i) := r.outputs.elim (M.oki ok);
@@ -57,13 +55,13 @@ def Multi.Transforms (M : Multi α) (x y : Tapes M.k M.τ) : Prop :=
 
 /-- Closure (reflexive+transitive) of `M.Transforms` with step counting. Predicate `M.Derives s t n` means
     the multi-tape semi-Thue system `M` can transform `s` to `t` in exactly `n` rewriting step. -/
-inductive Multi.Derives (M : Multi α) : Tapes M.k M.τ → Tapes M.k M.τ → ℕ → Prop
-  | refl (x : Tapes M.k M.τ) : M.Derives x x 0
-  | tail (x y z : Tapes M.k M.τ) (n : ℕ) : M.Derives x y n → M.Transforms y z → M.Derives x z n.succ
+inductive Multi.Derives (M : Multi α) : Tapes M.e M.τ → Tapes M.e M.τ → ℕ → Prop
+  | refl (x : Tapes M.e M.τ) : M.Derives x x 0
+  | tail (x y z : Tapes M.e M.τ) (n : ℕ) : M.Derives x y n → M.Transforms y z → M.Derives x z n.succ
 
 /-- Language recognized by given multi-tape semi-Thue system. -/
 def Multi.Semidecides (M : Multi α) : Language α :=
-  { w | ∃ n : ℕ, ∃ s : Tapes M.k M.τ, M.Derives (M.initiate w) s n ∧ M.terminate s }
+  { w | ∃ n : ℕ, ∃ s : Tapes M.e M.τ, M.Derives (M.initialize w) s n ∧ M.terminate s }
 
 /-- Definition of recursively-enumerable languages in terms of multi-tape semi-Thue systems. -/
 def Language.InMRE (L : Language α) : Prop :=
@@ -72,5 +70,5 @@ def Language.InMRE (L : Language α) : Prop :=
 
 /-- Semantic definition of deterministic computation. -/
 def Multi.IsDeterministic (M : Multi α) : Prop :=
-  ∀ w : List α, ∀ n : ℕ, ∀ s : Tapes M.k M.τ, M.Derives (M.initiate w) s n →
-    ∀ x y : Tapes M.k M.τ, M.Transforms s x ∧ M.Transforms s y → x = y
+  ∀ w : List α, ∀ n : ℕ, ∀ s : Tapes M.e M.τ, M.Derives (M.initialize w) s n →
+    ∀ x y : Tapes M.e M.τ, M.Transforms s x ∧ M.Transforms s y → x = y
--- Thue/Multitape/Examples.lean
+++ Thue/Multitape/Examples.lean
@@ -7,7 +7,7 @@ private def tau : ℕ → Type
 | .zero => Option (Fin 2)
 | .succ _ => Fin 8
 
-private def tauRule (u₁ u₂ : List (Option (Fin 2))) (v₁ v₂ : List (Fin 8)) : Mrule 2 tau :=
+private def tauRule (u₁ u₂ : List (Option (Fin 2))) (v₁ v₂ : List (Fin 8)) : Mrule 1 tau :=
   Mrule.mk (u₁, v₁, ()) (u₂, v₂, ())
 
 /-
@@ -20,46 +20,48 @@ Succeed:
 45
 -/
 
-private def move0 : Mrule 2 tau :=
+private def move0 : Mrule 1 tau :=
   tauRule [none, some 0] [none] [2] [0, 2]
 
-private def move1 : Mrule 2 tau :=
+private def move1 : Mrule 1 tau :=
   tauRule [none, some 1] [none] [2] [1, 2]
 
-private def uturn : Mrule 2 tau :=
+private def uturn : Mrule 1 tau :=
   tauRule [none, none] [none, none] [2] [3]
 
-private def rewind0 : Mrule 2 tau :=
+private def rewind0 : Mrule 1 tau :=
   tauRule [none, none] [none, none] [0, 3] [3]
 
-private def rewind1 : Mrule 2 tau :=
+private def rewind1 : Mrule 1 tau :=
   tauRule [none, none] [none, none] [1, 3] [3]
 
-private def ahead : Mrule 2 tau :=
+private def ahead : Mrule 1 tau :=
   tauRule [none, none] [none] [6, 3] [4]
 
-private def check0 : Mrule 2 tau :=
+private def check0 : Mrule 1 tau :=
   tauRule [none, some 0] [none] [4, 0] [4]
 
-private def check1 : Mrule 2 tau :=
+private def check1 : Mrule 1 tau :=
   tauRule [none, some 1] [none] [4, 1] [4]
 
-private def endYes : Mrule 2 tau :=
+private def endYes : Mrule 1 tau :=
   tauRule [none] [] [4, 5] [7]
 
 
 private def rulesRep := [move0, move1, uturn, rewind0, rewind1, ahead, check0, check1, endYes]
 
 private def machineRep : Multi (Option (Fin 2)) :=
-  Multi.mk 2 (Nat.succ_pos 1) tau id rulesRep ([none], [2, 2, 5], ()) (none, some 7, ())
+  Multi.mk 1 tau id rulesRep ([none], [2, 2, 5], ()) (none, some 7, ())
 
 private lemma easyDirection {w : List (Option (Fin 2))} {v : List (Fin 2)}
     (hyp: let v' := List.map some v ; v' ++ [none] ++ v' = w) :
   Multi.Derives machineRep
-    (Multi.initiate machineRep w)
+    (Multi.initialize machineRep w)
     (([] : List (Option (Fin 2))), ([7] : List (Fin 8)), ())
     666 :=
 by
+  unfold Multi.initialize
+  simp
   sorry
 
 private theorem langRepetition_is_RE : (langRepetition (Fin 2)).InMRE :=
