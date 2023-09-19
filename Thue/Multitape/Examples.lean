c0d04b019c0a35c924b1941c3c8268c6a8adfed9
madvorak <martin.dvorak@matfyz.cz>
2023-09-19 14:00:11 +0200

    refactoring

--- Thue/Multitape/Examples.lean
+++ Thue/Multitape/Examples.lean
@@ -14,11 +14,11 @@ private def tauRule (u₁ u₂ : List (Option (Fin 2))) (v₁ v₂ : List (Fin 8
 /-
 Start:
 ∅w = ∅v∅v
-625
+526
 
 Succeed:
 ∅
-45
+46
 -/
 
 private def move0 : Mrule 1 tau :=
@@ -37,7 +37,7 @@ private def rewind1 : Mrule 1 tau :=
   tauRule [none, none] [none, none] [1, 3] [3]
 
 private def ahead : Mrule 1 tau :=
-  tauRule [none, none] [none] [6, 3] [4]
+  tauRule [none, none] [none] [5, 3] [4]
 
 private def check0 : Mrule 1 tau :=
   tauRule [none, none, some 0] [none] [4, 0] [4]
@@ -46,58 +46,58 @@ private def check1 : Mrule 1 tau :=
   tauRule [none, none, some 1] [none] [4, 1] [4]
 
 private def endYes : Mrule 1 tau :=
-  tauRule [none, none] [none] [4, 5] [7]
+  tauRule [none, none] [none] [4, 6] [7]
 
 
 private def rulesRep := [move0, move1, uturn, rewind0, rewind1, ahead, check0, check1, endYes]
 
 private def machineRep : Multi (Option (Fin 2)) :=
-  Multi.mk 1 tau id rulesRep ([none], [6, 2, 5], ()) (none, some 7, ())
+  Multi.mk 1 tau id rulesRep ([none], [5, 2, 6], ()) (none, some 7, ())
 
 
 private def emb2fin8 : Fin 2 → Fin 8 := Fin.castLE (by decide)
 
-private lemma firstEpoch {v : List (Fin 2)} :
+private lemma epochScan {v : List (Fin 2)} :
   machineRep.Derives
-    (none :: (List.map some v ++ [none] ++ List.map some v), machineRep.starting.snd)
-    ([none, none] ++ List.map some v, 6 :: List.map emb2fin8 v ++ [2, 5], ())
+    ([none] ++ List.map some v ++ [none] ++ List.map some v, ([5, 2, 6] : List (Fin 8)), ())
+    ([none, none] ++ List.map some v, [5] ++ List.map emb2fin8 v ++ [2, 6], ())
     v.length :=
 by
   sorry
 
 private lemma stepUturn {v : List (Fin 2)} :
   machineRep.Transforms
-    ([none, none] ++ List.map some v, 6 :: List.map emb2fin8 v ++ [2, 5], ())
-    ([none, none] ++ List.map some v, 6 :: List.map emb2fin8 v ++ [3, 5], ()) :=
+    ([none, none] ++ List.map some v, [5] ++ List.map emb2fin8 v ++ [2, 6], ())
+    ([none, none] ++ List.map some v, [5] ++ List.map emb2fin8 v ++ [3, 6], ()) :=
 by
   sorry
 
-private lemma secondEpoch {v : List (Fin 2)} :
+private lemma epochRewind {v : List (Fin 2)} :
   machineRep.Derives
-    ([none, none] ++ List.map some v, [6] ++ List.map emb2fin8 v ++ [3, 5], ())
-    ([none, none] ++ List.map some v, [6, 3] ++ List.map emb2fin8 v ++ [5], ())
+    ([none, none] ++ List.map some v, [5] ++ List.map emb2fin8 v ++ [3, 6], ())
+    ([none, none] ++ List.map some v, [5, 3] ++ List.map emb2fin8 v ++ [6], ())
     v.length :=
 by
   sorry
 
 private lemma stepAhead {v : List (Fin 2)} :
   machineRep.Transforms
-    ([none, none] ++ List.map some v, [6, 3] ++ List.map emb2fin8 v ++ [5], ())
-    ([none, none] ++ List.map some v, [4] ++ List.map emb2fin8 v ++ [5], ()) :=
+    ([none, none] ++ List.map some v, [5, 3] ++ List.map emb2fin8 v ++ [6], ())
+    ([none, none] ++ List.map some v, [4] ++ List.map emb2fin8 v ++ [6], ()) :=
 by
   sorry
 
-private lemma thirdEpoch {v : List (Fin 2)} :
+private lemma epochCheck {v : List (Fin 2)} :
   machineRep.Derives
-    ([none, none] ++ List.map some v, [4] ++ List.map emb2fin8 v ++ [5], ())
-    ([none, none], ([4, 5] : List (Fin 8)), ())
+    ([none, none] ++ List.map some v, [4] ++ List.map emb2fin8 v ++ [6], ())
+    ([none, none], ([4, 6] : List (Fin 8)), ())
     v.length :=
 by
   sorry
 
 private lemma stepYes :
   machineRep.Transforms
-    (([none, none] : List (Option (Fin 2))), ([4, 5] : List (Fin 8)), ())
+    (([none, none] : List (Option (Fin 2))), ([4, 6] : List (Fin 8)), ())
     (([none] : List (Option (Fin 2))), ([7] : List (Fin 8)), ()) :=
 by
   use endYes
@@ -119,24 +119,21 @@ private lemma easyDirection {w : List (Option (Fin 2))} {v : List (Fin 2)}
     (([none] : List (Option (Fin 2))), ([7] : List (Fin 8)), ())
     (3 * v.length + 3) :=
 by
-  show
-    machineRep.Derives
-      (machineRep.starting.fst.append (List.map machineRep.embed w), machineRep.starting.snd)
-      ([none], ([7] : List (Fin 8)), ())
-      (3 * List.length v + 3)
   convert_to
     machineRep.Derives
-      (none :: w, machineRep.starting.snd)
+      ([none] ++ w, machineRep.starting.snd)
       ([none], ([7] : List (Fin 8)), ())
       (v.length + 1 + v.length + 1 + v.length + 1)
-  · simp [machineRep]
+  · show (machineRep.starting.fst.append (w.map machineRep.embed), _) = _
+    simp [machineRep]
   · ring
   rw [←hyp]
   apply Multi.deri_of_deri_tran _ stepYes
-  apply Multi.deri_of_deri_deri _ thirdEpoch
+  apply Multi.deri_of_deri_deri _ epochCheck
   apply Multi.deri_of_deri_tran _ stepAhead
-  apply Multi.deri_of_deri_deri _ secondEpoch
-  apply Multi.deri_of_deri_tran firstEpoch stepUturn
+  apply Multi.deri_of_deri_deri _ epochRewind
+  apply Multi.deri_of_deri_tran _ stepUturn
+  exact epochScan -- no idea why `exact` works and why `convert` doesn't
 
 private theorem langRepetition_is_RE : (langRepetition (Fin 2)).InMRE :=
 by
